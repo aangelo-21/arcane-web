47ef70b673740d946ad8ebd5c636dbcac0866fbf
aangelo-21 <martinezglielmo@gmail.com>
2024-12-02 15:42:03 +0000

    validated form

---
+++ js/validation.js
@@ -0,0 +1,34 @@
+function listenForValidation() {
+  const FAN_ZONE_FORM = document.getElementById("fan-zone-form");
+  FAN_ZONE_FORM.addEventListener("submit", validateFanZoneForm);
+}
+
+function validateFanZoneForm(e) {
+
+  const NAME = e.target.name.value;
+  const SUBJECT = e.target.subject.value;
+  const MESSAGE = e.target.message.value;
+
+  let valid = true;
+
+  if (!NAME) {
+    document.getElementById("span-name").style.visibility = "visible";
+    valid = false;
+  } else document.getElementById("span-name").style.visibility = "hidden";
+
+  if (!SUBJECT) {
+    document.getElementById("span-subject").style.visibility = "visible";
+    valid = false;
+  } else document.getElementById("span-subject").style.visibility = "hidden";
+
+  if (!MESSAGE) {
+    document.getElementById("span-message").style.visibility = "visible";
+    valid = false;
+  } else document.getElementById("span-message").style.visibility = "hidden";
+
+  if (!valid) {
+    e.preventDefault();
+  }
+}
+
+listenForValidation();
